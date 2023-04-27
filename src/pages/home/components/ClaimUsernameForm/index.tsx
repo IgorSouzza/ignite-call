de63d476152226dbeaaa684dc0e879f77acd4f51
Igor Souza <igor.souza.webmaster@gmail.com>
2023-04-26 23:08:30 -0300

    feat: add home page

---
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -0,0 +1,58 @@
+import { useForm } from 'react-hook-form'
+import { ArrowRight } from 'phosphor-react'
+import { Button, Text, TextInput } from '@ignite-ui/react'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+
+import { Form, FormAnnotation } from './styles'
+
+const claumUsernameFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: 'O usuário precisa ter pelo menos 3 letras.' })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: 'O usuário pode ter apernas letras e hifens',
+    })
+    .transform((username) => username.toLowerCase()),
+})
+
+type ClaumUsernameFormData = z.infer<typeof claumUsernameFormSchema>
+
+export function ClaumUsernameForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ClaumUsernameFormData>({
+    resolver: zodResolver(claumUsernameFormSchema),
+  })
+
+  function handleClaimUsername(data: ClaumUsernameFormData) {
+    console.log(data)
+  }
+
+  return (
+    <>
+      <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+        <TextInput
+          size="sm"
+          prefix="ignite.com/"
+          placeholder="seu-usuario"
+          autoComplete="off"
+          {...register('username')}
+        />
+        <Button size="sm" type="submit">
+          Reservar
+          <ArrowRight />
+        </Button>
+      </Form>
+      <FormAnnotation>
+        <Text size="sm">
+          {errors.username
+            ? errors.username.message
+            : 'Digite o nome do usuário desejado'}
+        </Text>
+      </FormAnnotation>
+    </>
+  )
+}
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,3 +1,1 @@
-export default function Home() {
-  return <div>Hello</div>
-}
+export { default } from './home'
